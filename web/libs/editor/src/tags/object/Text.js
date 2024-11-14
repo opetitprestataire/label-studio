a535216d861e86c86f9e1681bd4907012a94189a
makseq-ubnt <makseq@gmail.com>
2024-11-14 00:51:28 +0000

    biome

--- web/libs/editor/src/tags/object/Text.js
+++ web/libs/editor/src/tags/object/Text.js
@@ -7,7 +7,7 @@
  *
  * Use with the following data types: text.
  *
- * ### How to read my text files in python? 
+ * ### How to read my text files in python?
  * The Label Studio editor counts `\r\n` as two different symbols, displaying them as `\n\n`, making it look like there is extra margin between lines.
  * You should either preprocess your files to replace `\r\n` with `\n` completely, or open files in Python with `newline=''` to avoid converting `\r\n` to `\n`:
  * `with open('my-file.txt', encoding='utf-8', newline='') as f: text = f.read()`
