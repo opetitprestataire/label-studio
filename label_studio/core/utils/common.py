837bba9188b38176870e307500638041f41a7b05
hakan458 <hakan@heartex.com>
2025-01-13 13:20:34 -0800

    change to django timezone.now()

--- label_studio/core/utils/common.py
+++ label_studio/core/utils/common.py
@@ -14,7 +14,6 @@ import time
 import traceback as tb
 import uuid
 from collections import defaultdict
-from datetime import datetime, timezone
 from functools import wraps
 from typing import Any, Callable, Generator, Iterable, Mapping, Optional
 
@@ -40,7 +39,7 @@ from django.db.models.signals import (
     pre_migrate,
     pre_save,
 )
-from django.db.utils import OperationalError
+from django.db.utils import OperationalError, timezone
 from django.utils.crypto import get_random_string
 from django.utils.module_loading import import_string
 from django_filters.rest_framework import DjangoFilterBackend
@@ -261,7 +260,7 @@ def datetime_to_timestamp(dt):
 
 
 def timestamp_now():
-    return datetime_to_timestamp(datetime.now(timezone.utc))
+    return datetime_to_timestamp(timezone.now())
 
 
 def find_first_one_to_one_related_field_by_prefix(instance, prefix):
