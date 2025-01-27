9bd1db06bfb5d23c459ec4a73c3ad38671393b16
Brandon Martel <brandonmartel@gmail.com>
2025-01-27 11:37:32 -0600

    remove the development override for home page FF

--- web/apps/labelstudio/src/utils/feature-flags.ts
+++ web/apps/labelstudio/src/utils/feature-flags.ts
@@ -44,7 +44,6 @@ export function isFF(id: string) {
   // TODO: remove the override + if statement once LSE and LSO start building react the same way and fflag_fix_front_lsdv_4620_memory_leaks_100723_short is removed
   const override: Record<string, boolean> = {
     fflag_fix_front_lsdv_4620_memory_leaks_100723_short: false,
-    [FF_HOMEPAGE]: true,
   };
   if (window?.APP_SETTINGS?.sentry_environment === "opensource" && id in override) {
     return override[id];
