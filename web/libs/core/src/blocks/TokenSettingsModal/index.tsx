f1998d83b8f2e4ecf7553a9e964c2a117a2e455d
Nick Skriabin <767890+nick-skriabin@users.noreply.github.com>
2025-02-19 13:57:38 +0000

    Update web/libs/core/src/pages/AccountSettings/sections/MembershipInfo.tsx
    Co-authored-by: yyassi-heartex <104568407+yyassi-heartex@users.noreply.github.com>

--- web/libs/core/src/blocks/TokenSettingsModal/index.tsx
+++ web/libs/core/src/blocks/TokenSettingsModal/index.tsx
@@ -1,10 +1,13 @@
-import { Form, Input, Toggle } from "apps/labelstudio/src/components/Form";
 import { useAtomValue } from "jotai";
 import { formDataToJPO } from "@humansignal/core/lib/utils/helpers";
 import { saveSettingsAtom, settingsAtom } from "@humansignal/core/pages/AccountSettings/atoms";
 import type { AuthTokenSettings } from "@humansignal/core/pages/AccountSettings/types";
-import { useRef } from "react";
-import { debounce } from "@humansignal/core/lib/utils/debounce";
+import { useEffect, useRef, useState } from "react";
+import styles from "./styles.module.scss";
+
+import { Form, Input, Toggle } from "apps/labelstudio/src/components/Form";
+import { Button } from "apps/labelstudio/src/components/Button/Button";
+import { IconCheck } from "apps/labelstudio/src/assets/icons";
 
 export const TokenSettingsModal = ({
   showTTL,
@@ -13,18 +16,27 @@ export const TokenSettingsModal = ({
 }) => {
   const settings = useAtomValue(settingsAtom);
   const formRef = useRef<Form>();
-  const { mutate: saveSettings } = useAtomValue(saveSettingsAtom);
-  const onSubmit = debounce(() => {
+  const { mutate: saveSettings, isPending, isSuccess, isIdle } = useAtomValue(saveSettingsAtom);
+  const [saved, setSaved] = useState(false);
+  const onSubmit = () => {
     const form = formRef.current.formElement.current as HTMLFormElement;
     const data = formDataToJPO(new FormData(form));
     const body = {
+      ...data,
       api_tokens_enabled: data.api_tokens_enabled === "on",
       legacy_api_tokens_enabled: data.legacy_api_tokens_enabled === "on",
     } satisfies Partial<AuthTokenSettings>;
     saveSettings(body);
-  });
+    setSaved(true);
+  };
+
+  useEffect(() => {
+    if (saved) {
+      setTimeout(() => setSaved(false), 2000);
+    }
+  }, [saved]);
   return (
-    <Form ref={formRef} onChange={onSubmit}>
+    <Form ref={formRef}>
       <Form.Row columnCount={1}>
         <Toggle
           label="Personal Access Tokens"
@@ -44,7 +56,7 @@ export const TokenSettingsModal = ({
       {showTTL === true && (
         <Form.Row columnCount={1}>
           <Input
-            name="time_to_live"
+            name="api_token_ttl_days"
             label="Personal Access Token Time-to-Live"
             description="The number of days, after creation, that the token will be valid for. After this time period a user will need to create a new access token"
             labelProps={{
@@ -59,6 +71,16 @@ export const TokenSettingsModal = ({
           />
         </Form.Row>
       )}
+      <Form.Actions>
+        {saved && (
+          <div className={styles.success}>
+            <IconCheck /> Saved!
+          </div>
+        )}
+        <Button type="button" onClick={onSubmit} disabled={isPending || saved}>
+          Save
+        </Button>
+      </Form.Actions>
     </Form>
   );
 };
