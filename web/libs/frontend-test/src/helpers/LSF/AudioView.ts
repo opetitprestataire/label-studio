474ab71583d5ba87165d1a84babae303f7a09f50
Gondragos <gondragos@gmail.com>
2025-03-06 19:48:54 +0000

    fix: adjust comment regarding `isReady` timing in AudioView

--- web/libs/frontend-test/src/helpers/LSF/AudioView.ts
+++ web/libs/frontend-test/src/helpers/LSF/AudioView.ts
@@ -40,9 +40,7 @@ export const AudioView = {
     LabelStudio.waitForObjectsReady();
     this.loadingBar.should("not.exist");
     /**
-     * In `AudioUltra` version of the `Audio` tag isReady was missed.
-     * It's fixed right now, but it was done in the easiest way it could be done.
-     * In that case, there is still a time gap between setting `isReady` to `true` and getting the last initial draw at the canvas,
+     * There is a time gap between setting `isReady` to `true` and getting the last initial draw at the canvas,
      * which for now we are going to compensate by waiting approximately 2 frames of render (16 * 2 = 32 milliseconds)
      * @todo: remove wait when `isReady` in audio become more precise
      */
