33e43aca319cce047470dd345c5239177b789bfb
Brandon Martel <brandonmartel@gmail.com>
2025-03-18 07:03:39 -0500

    linting

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,27 +1,3 @@
-const baseConfig = require("./libs/ui/src/tailwind.config");
-let designTokens = {};
+const twConfig = require("./libs/ui/src/tailwind.config");
 
-try {
-  designTokens = require("./libs/ui/src/tokens/tokens.js");
-} catch (error) {
-  console.warn("Design tokens not found. Run 'nx design-tokens ui' to generate them.");
-  // Use empty object if tokens file doesn't exist yet
-  designTokens = { colors: {} };
-}
-
-// Merge the design tokens with the base config
-const mergedConfig = {
-  ...baseConfig,
-  theme: {
-    ...baseConfig.theme,
-    extend: {
-      ...baseConfig.theme.extend,
-      colors: {
-        ...baseConfig.theme.extend.colors,
-        ...designTokens.colors,
-      },
-    },
-  },
-};
-
-module.exports = mergedConfig;
+module.exports = twConfig;
