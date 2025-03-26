f3dd630e175d6166c0d20077d676e4e2fd785db1
Nick Skriabin <nr@fenelon.ru>
2025-03-26 17:20:32 +0000

    Handle the Failed to fetch error

--- web/libs/core/src/lib/api-proxy/index.ts
+++ web/libs/core/src/lib/api-proxy/index.ts
@@ -15,6 +15,14 @@ type ApiMethods<T extends Record<string, EndpointConfig>> = {
   [K in keyof T]: (params?: Record<string, any>, options?: ApiParams) => Promise<WrappedResponse<any>>;
 };
 
+// We're catching certain types of errors that
+// are not supposed to be user-facing
+const IGNORED_ERRORS = new Set([
+  "AbortError", // Whenever the abort controller kicked in
+  "Failed to fetch", // Network's offline, bad URL, CORS, etc. in Chrome
+  "NetworkError when attempting to fetch resource.", // Same as the above but Firefox,
+]);
+
 export type ApiParams = {
   headers?: RequestInit["headers"];
   signal?: RequestInit["signal"];
@@ -221,14 +229,14 @@ export class APIProxy<T extends {}> {
         } else {
           try {
             rawResponse = await fetch(apiCallURL, requestParams);
-          } catch (e: any) {
+          } catch (err: any) {
             // we must catch an AbortError here to as it's fired if
             // the request is aborted mid-flight
-            if (e instanceof DOMException && e.name === "AbortError") {
+            if (IGNORED_ERRORS.has(err.message)) {
               return null;
             }
-            responseResult = this.generateException(e as Error);
-            rawResponse = new Response(`${e.name}: ${e.message}`, { status: 500 });
+            responseResult = this.generateException(err as Error);
+            return new Response(`${err.name}: ${err.message}`, { status: 500 });
           }
         }
 
