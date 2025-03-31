cb707fb4af9ecbdce17e55cf256cec3d478af717
Yousif Yassi <yousif@heartex.com>
2025-03-31 11:42:03 -0400

    adding data-value attribute to select trigger

--- web/libs/ui/src/lib/select/select.tsx
+++ web/libs/ui/src/lib/select/select.tsx
@@ -139,6 +139,7 @@ export const Select = forwardRef(
             data-testid={props?.dataTestid ?? "select-trigger"}
             ref={triggerRef}
             data-name={props?.name}
+            data-value={value ?? ""}
             {...triggerProps}
           >
             <span className="flex flex-1 text-left gap-2 leading-none" data-testid="select-display-value">
