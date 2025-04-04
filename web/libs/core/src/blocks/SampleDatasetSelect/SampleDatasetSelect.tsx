211fc6808ea55ab2991399f1050aeaadec258dab
Nick Skriabin <nr@fenelon.ru>
2025-04-04 14:09:37 +0100

    UI improvements to the Sample dataset select

--- web/libs/core/src/blocks/SampleDatasetSelect/SampleDatasetSelect.tsx
+++ web/libs/core/src/blocks/SampleDatasetSelect/SampleDatasetSelect.tsx
@@ -1,4 +1,4 @@
-import { Select, SelectContent, SelectItem, SelectTrigger } from "@humansignal/shad/components/ui/select";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger } from "@humansignal/shad/components/ui/select";
 import { useCallback, useMemo } from "react";
 
 type Sample = {
@@ -30,17 +30,19 @@ export function SampleDatasetSelect({
   return (
     <div className="flex gap-3 items-center">
       <span className="text-neutral-content-subtler">or use a sample dataset</span>
-      <Select value={sample?.url ?? undefined} onValueChange={onSelect}>
-        <SelectTrigger className="h-10 min-w-52 rounded-sm border-neutral-border-bold data-[placeholder]:text-[#000] text-[16px] [&_svg]:stroke-[#000]">
+      <Select value={sample?.url ?? null} onValueChange={onSelect}>
+        <SelectTrigger className="!h-10 min-w-52 rounded-sm border-neutral-border-bold data-[placeholder]:!text-[#000] data-[placeholder]:text-[16px] text-[16px] [&_svg]:stroke-[#000]">
           {title}
         </SelectTrigger>
-        <SelectContent className="z-99999 min-w-90">
-          {samples.map((sample) => (
-            <SelectItem value={sample.url} key={sample.url}>
-              <div className=" font-bold">{sample.title}</div>
-              <div className="mt-2">{sample.description}</div>
-            </SelectItem>
-          ))}
+        <SelectContent className="z-99999 min-w-90 p-2">
+          <SelectGroup>
+            {samples.map((sample) => (
+              <SelectItem value={sample.url} key={sample.url} className="hover:bg-neutral-surface-active">
+                <div className=" font-bold">{sample.title}</div>
+                <div className="mt-2">{sample.description}</div>
+              </SelectItem>
+            ))}
+          </SelectGroup>
         </SelectContent>
       </Select>
     </div>
