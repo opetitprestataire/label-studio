d4ffc7837185584a446f2a9fa94a38b0f48fe34e
Brandon Martel <brandonmartel@gmail.com>
2025-04-09 09:12:28 -0500

    makeshift an auto icon for storybook theme toggle

--- web/libs/storybook/addons/theme-toggle/theme-toggle.tsx
+++ web/libs/storybook/addons/theme-toggle/theme-toggle.tsx
@@ -28,8 +28,52 @@ export const ThemeTool = React.memo(function MyAddonSelector() {
   }, [toggleTheme, api]);
 
   return (
-    <IconButton key={TOOL_ID} active={theme !== DEFAULT_THEME} title="Toggle theme" onClick={toggleTheme}>
-      {evaluatedTheme === "dark" ? <MoonIcon /> : <SunIcon />}
+    <IconButton
+      style={{ height: "28px", width: "28px" }}
+      key={TOOL_ID}
+      active={theme !== DEFAULT_THEME}
+      title="Toggle theme"
+      onClick={toggleTheme}
+    >
+      {theme === DEFAULT_THEME ? (
+        <div style={{ position: "relative" }}>
+          {evaluatedTheme === "light" ? (
+            <>
+              <SunIcon style={{ top: "50%", left: "50%", position: "absolute", transform: "translate(-50%, -50%)" }} />
+              <MoonIcon
+                style={{
+                  height: "8px",
+                  width: "8px",
+                  opacity: "0.5",
+                  top: "50%",
+                  left: "50%",
+                  position: "absolute",
+                  transform: "translate(80%, -120%)",
+                }}
+              />
+            </>
+          ) : (
+            <>
+              <MoonIcon style={{ top: "50%", left: "50%", position: "absolute", transform: "translate(-50%, -50%)" }} />
+              <SunIcon
+                style={{
+                  height: "10px",
+                  width: "10px",
+                  opacity: "0.5",
+                  top: "50%",
+                  left: "50%",
+                  position: "absolute",
+                  transform: "translate(40%, -100%)",
+                }}
+              />
+            </>
+          )}
+        </div>
+      ) : evaluatedTheme === "dark" ? (
+        <MoonIcon />
+      ) : (
+        <SunIcon />
+      )}
     </IconButton>
   );
 });
