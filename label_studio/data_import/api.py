6698b3f5a8209b9ef319949b43a19ca28d75109a
makseq <makseq@gmail.com>
2025-04-09 02:59:40 +0100

    Fix

--- label_studio/data_import/api.py
+++ label_studio/data_import/api.py
@@ -816,7 +816,7 @@ class PresignAPIMixin:
         # Proxy to presigned url
         response = HttpResponseRedirect(redirect_to=url, status=status.HTTP_303_SEE_OTHER)
         response.headers['Cache-Control'] = f'no-store, max-age={max_age}'
-        
+
         return response
             
     def proxy_data_from_storage(self, request, uri, storage):
