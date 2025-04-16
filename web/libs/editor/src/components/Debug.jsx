a9db33062fd9907c16c860a98254f6d553a9aa4d
Nick Skriabin <nr@fenelon.ru>
2025-04-16 15:51:01 +0100

    Update antd buttons (WIP)

--- web/libs/editor/src/components/Debug.jsx
+++ web/libs/editor/src/components/Debug.jsx
@@ -1,5 +1,6 @@
 import { useCallback, useRef } from "react";
-import { Button, Form } from "antd";
+import { Form } from "antd";
+import { Button } from "@humansignal/ui";
 
 import { observer } from "mobx-react";
 
@@ -57,9 +58,15 @@ const DebugComponent = ({ store }) => {
       <br />
       <h2>Debug</h2>
       <div>
-        <Button onClick={serializeAll}>↓ Serialize All Annotations</Button>
-        <Button onClick={serializeCurrent}>↓ Serialize Current Annotation</Button>
-        <Button onClick={loadTask}>↑ Simulate Loading Task</Button>
+        <Button size="small" onClick={serializeAll}>
+          ↓ Serialize All Annotations
+        </Button>
+        <Button size="small" onClick={serializeCurrent}>
+          ↓ Serialize Current Annotation
+        </Button>
+        <Button size="small" onClick={loadTask}>
+          ↑ Simulate Loading Task
+        </Button>
       </div>
 
       <Form>
