3d3d5eb7b539a105338d3be22b2d2a1c6c9f620d
Nick Skriabin <nr@fenelon.ru>
2025-04-22 20:48:52 +0100

    Include libs in tailwind compilation process

--- web/libs/ui/src/tailwind.config.js
+++ web/libs/ui/src/tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
   content: [
     "./apps/**/*.{js,jsx,ts,tsx}",
     "./libs/ui/src/**/*.{js,jsx,ts,tsx}",
+    "./libs/libs/editor/src/**/*.{js,jsx,ts,tsx}",
+    "./libs/libs/datamanager/src/**/*.{js,jsx,ts,tsx}",
     "./libs/core/src/**/*.{js,jsx,ts,tsx}",
     "./libs/storybook/**/*.{js,jsx,ts,tsx}",
   ],
