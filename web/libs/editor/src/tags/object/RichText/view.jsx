002b0956dfb52ccf2ad96308321868d19bb68e41
hlomzik <hlomzik@gmail.com>
2025-04-23 14:14:08 +0100

    Fix linting

--- web/libs/editor/src/tags/object/RichText/view.jsx
+++ web/libs/editor/src/tags/object/RichText/view.jsx
@@ -5,7 +5,12 @@ import * as xpath from "xpath-range";
 import { inject, observer } from "mobx-react";
 import { STATE_CLASS_MODS } from "../../../mixins/HighlightMixin";
 import Utils from "../../../utils";
-import { applyTextGranularity, fixCodePointsInRange, rangeToGlobalOffset, trimSelection } from "../../../utils/selection-tools";
+import {
+  applyTextGranularity,
+  fixCodePointsInRange,
+  rangeToGlobalOffset,
+  trimSelection,
+} from "../../../utils/selection-tools";
 import "./RichText.scss";
 import { isAlive } from "mobx-state-tree";
 import { LoadingOutlined } from "@ant-design/icons";
