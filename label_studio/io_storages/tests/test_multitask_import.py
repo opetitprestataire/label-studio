414a053ef2d540f2d8bc017c60b04f1626ed1931
Matt Bernstein <matt@humansignal.com>
2025-04-30 14:57:19 -0400

    ruff

--- label_studio/io_storages/tests/test_multitask_import.py
+++ label_studio/io_storages/tests/test_multitask_import.py
@@ -11,7 +11,6 @@ from io_storages.tests.factories import (
 from moto import mock_s3
 from projects.tests.factories import ProjectFactory
 from rest_framework.test import APIClient
-
 from tests.utils import azure_client_mock, gcs_client_mock, redis_client_mock
 
 
