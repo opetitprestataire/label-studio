cf6ae40021b0ed3a8ed681d62c9eaaa92fece674
Nick Skriabin <nr@fenelon.ru>
2025-05-05 11:46:00 +0100

    Add missing exports

--- web/libs/ui/src/index.ts
+++ web/libs/ui/src/index.ts
@@ -13,6 +13,7 @@ export * from "./lib/code-editor/code-editor";
 export * from "./lib/enterprise-badge/enterprise-badge";
 export * from "./lib/label/label";
 export * from "./lib/select/select";
+export * from "./lib/skeleton/skeleton";
 export * from "./lib/toast/toast";
 export * from "./lib/toggle/toggle";
 
@@ -20,3 +21,5 @@ export * from "./assets/icons";
 export * from "./lib/simple-card";
 export * from "./lib/space/space";
 export * from "./lib/spinner/spinner";
+
+export { cn } from "./shad/utils";
