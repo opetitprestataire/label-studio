0d61649ff3a3e8d76aebaeee200389eb9567b631
Brandon Martel <brandonmartel@gmail.com>
2025-05-06 16:06:12 -0500

    make the diff easier

--- web/libs/datamanager/src/components/Common/Dropdown/DropdownComponent.jsx
+++ web/libs/datamanager/src/components/Common/Dropdown/DropdownComponent.jsx
@@ -10,164 +10,162 @@ import { DropdownTrigger } from "./DropdownTrigger";
 
 let lastIndex = 1;
 
-export const Dropdown = React.forwardRef(
-  ({ animated = true, visible = false, constrainHeight = false, ...props }, ref) => {
-    const rootName = cn("dropdown-dm");
+export const Dropdown = React.forwardRef(({ animated = true, visible = false, ...props }, ref) => {
+  const rootName = cn("dropdown-dm");
 
-    /**@type {import('react').RefObject<HTMLElement>} */
-    const dropdown = React.useRef();
-    const { triggerRef } = React.useContext(DropdownContext) ?? {};
-    const isInline = triggerRef === undefined;
+  /**@type {import('react').RefObject<HTMLElement>} */
+  const dropdown = React.useRef();
+  const { triggerRef } = React.useContext(DropdownContext) ?? {};
+  const isInline = triggerRef === undefined;
 
-    const { children, align, openUpwardForShortViewport } = props;
-    const [currentVisible, setVisible] = React.useState(visible);
-    const [offset, setOffset] = React.useState({});
-    const [visibility, setVisibility] = React.useState(visible ? "visible" : null);
+  const { children, align, openUpwardForShortViewport, constrainHeight = false } = props;
+  const [currentVisible, setVisible] = React.useState(visible);
+  const [offset, setOffset] = React.useState({});
+  const [visibility, setVisibility] = React.useState(visible ? "visible" : null);
 
-    const calculatePosition = React.useCallback(() => {
-      const dropdownEl = dropdown.current;
-      const parent = triggerRef?.current ?? dropdownEl.parentNode;
-      const { left, top } = alignElements(
-        parent,
-        dropdownEl,
-        align ?? "bottom-left",
-        0,
-        constrainHeight,
-        openUpwardForShortViewport ?? true,
-      );
-
-      setOffset({ left, top });
-    }, [triggerRef, align, openUpwardForShortViewport]);
-
-    const dropdownIndex = React.useMemo(() => {
-      return lastIndex++;
-    }, []);
-
-    const performAnimation = React.useCallback(
-      async (visible = false) => {
-        if (props.enabled === false && visible === true) return;
-
-        return new Promise((resolve) => {
-          const menu = dropdown.current;
-
-          if (animated !== false) {
-            aroundTransition(menu, {
-              transition: () => {
-                setVisibility(visible ? "appear" : "disappear");
-              },
-              beforeTransition: () => {
-                setVisibility(visible ? "before-appear" : "before-disappear");
-              },
-              afterTransition: () => {
-                setVisibility(visible ? "visible" : null);
-                resolve();
-              },
-            });
-          } else {
-            setVisibility(visible ? "visible" : null);
-            resolve();
-          }
-        });
-      },
-      [animated],
+  const calculatePosition = React.useCallback(() => {
+    const dropdownEl = dropdown.current;
+    const parent = triggerRef?.current ?? dropdownEl.parentNode;
+    const { left, top } = alignElements(
+      parent,
+      dropdownEl,
+      align ?? "bottom-left",
+      0,
+      constrainHeight,
+      openUpwardForShortViewport ?? true,
     );
 
-    const close = React.useCallback(async () => {
-      if (currentVisible === false) return;
+    setOffset({ left, top });
+  }, [triggerRef, align, openUpwardForShortViewport]);
 
-      props.onToggle?.(false);
-      await performAnimation(false);
-      setVisible(false);
-    }, [currentVisible, performAnimation, props]);
+  const dropdownIndex = React.useMemo(() => {
+    return lastIndex++;
+  }, []);
 
-    const open = React.useCallback(async () => {
-      if (currentVisible === true) return;
+  const performAnimation = React.useCallback(
+    async (visible = false) => {
+      if (props.enabled === false && visible === true) return;
 
-      props.onToggle?.(true);
-      await performAnimation(true);
-      setVisible(true);
-    }, [currentVisible, performAnimation, props]);
+      return new Promise((resolve) => {
+        const menu = dropdown.current;
 
-    const toggle = React.useCallback(async () => {
-      const newState = !currentVisible;
+        if (animated !== false) {
+          aroundTransition(menu, {
+            transition: () => {
+              setVisibility(visible ? "appear" : "disappear");
+            },
+            beforeTransition: () => {
+              setVisibility(visible ? "before-appear" : "before-disappear");
+            },
+            afterTransition: () => {
+              setVisibility(visible ? "visible" : null);
+              resolve();
+            },
+          });
+        } else {
+          setVisibility(visible ? "visible" : null);
+          resolve();
+        }
+      });
+    },
+    [animated],
+  );
 
-      if (newState) {
-        open();
-      } else {
-        close();
-      }
-    }, [close, currentVisible, open]);
+  const close = React.useCallback(async () => {
+    if (currentVisible === false) return;
 
-    React.useEffect(() => {
-      if (!ref) return;
+    props.onToggle?.(false);
+    await performAnimation(false);
+    setVisible(false);
+  }, [currentVisible, performAnimation, props]);
 
-      ref.current = {
-        dropdown: dropdown.current,
-        visible: visibility !== null,
-        toggle,
-        open,
-        close,
-      };
-    }, [close, open, ref, toggle, dropdown, visibility]);
+  const open = React.useCallback(async () => {
+    if (currentVisible === true) return;
 
-    React.useEffect(() => {
-      setVisible(visible);
-    }, [visible]);
+    props.onToggle?.(true);
+    await performAnimation(true);
+    setVisible(true);
+  }, [currentVisible, performAnimation, props]);
 
-    React.useEffect(() => {
-      if (!isInline && visibility === "before-appear") {
-        calculatePosition();
-      }
-    }, [visibility, calculatePosition, isInline]);
+  const toggle = React.useCallback(async () => {
+    const newState = !currentVisible;
 
-    React.useEffect(() => {
-      if (props.enabled === false) performAnimation(false);
-    }, [props.enabled]);
+    if (newState) {
+      open();
+    } else {
+      close();
+    }
+  }, [close, currentVisible, open]);
 
-    const content =
-      children.props && children.props.type === "Menu"
-        ? React.cloneElement(children, {
-            ...children.props,
-            className: rootName.elem("menu").mix(children.props.className),
-          })
-        : children;
+  React.useEffect(() => {
+    if (!ref) return;
 
-    const visibilityClasses = React.useMemo(() => {
-      switch (visibility) {
-        case "before-appear":
-          return "before-appear";
-        case "appear":
-          return "appear before-appear";
-        case "before-disappear":
-          return "before-disappear";
-        case "disappear":
-          return "disappear before-disappear";
-        case "visible":
-          return "visible";
-        default:
-          return visible ? "visible" : null;
-      }
-    }, [visibility, visible]);
-
-    const compositeStyles = {
-      ...(props.style ?? {}),
-      ...(offset ?? {}),
-      zIndex: 1000 + dropdownIndex,
+    ref.current = {
+      dropdown: dropdown.current,
+      visible: visibility !== null,
+      toggle,
+      open,
+      close,
     };
-    const result = (
-      <div
-        ref={dropdown}
-        className={clsx(rootName.toString(), rootName.mix([props.className, visibilityClasses]).toString())}
-        style={compositeStyles}
-        onClick={(e) => e.stopPropagation()}
-      >
-        {content}
-      </div>
-    );
+  }, [close, open, ref, toggle, dropdown, visibility]);
 
-    return props.inline === true ? result : ReactDOM.createPortal(result, document.body);
-  },
-);
+  React.useEffect(() => {
+    setVisible(visible);
+  }, [visible]);
+
+  React.useEffect(() => {
+    if (!isInline && visibility === "before-appear") {
+      calculatePosition();
+    }
+  }, [visibility, calculatePosition, isInline]);
+
+  React.useEffect(() => {
+    if (props.enabled === false) performAnimation(false);
+  }, [props.enabled]);
+
+  const content =
+    children.props && children.props.type === "Menu"
+      ? React.cloneElement(children, {
+          ...children.props,
+          className: rootName.elem("menu").mix(children.props.className),
+        })
+      : children;
+
+  const visibilityClasses = React.useMemo(() => {
+    switch (visibility) {
+      case "before-appear":
+        return "before-appear";
+      case "appear":
+        return "appear before-appear";
+      case "before-disappear":
+        return "before-disappear";
+      case "disappear":
+        return "disappear before-disappear";
+      case "visible":
+        return "visible";
+      default:
+        return visible ? "visible" : null;
+    }
+  }, [visibility, visible]);
+
+  const compositeStyles = {
+    ...(props.style ?? {}),
+    ...(offset ?? {}),
+    zIndex: 1000 + dropdownIndex,
+  };
+  const result = (
+    <div
+      ref={dropdown}
+      className={clsx(rootName.toString(), rootName.mix([props.className, visibilityClasses]).toString())}
+      style={compositeStyles}
+      onClick={(e) => e.stopPropagation()}
+    >
+      {content}
+    </div>
+  );
+
+  return props.inline === true ? result : ReactDOM.createPortal(result, document.body);
+});
 
 Dropdown.displayName = "Dropdown";
 
