294d4663e7c86f8cbf83df836eb60a32bdaa98f6
Brandon Martel <brandonmartel@gmail.com>
2025-05-13 08:45:23 -0500

    fix annotation cleanup

--- web/libs/editor/src/components/App/Annotation.js
+++ web/libs/editor/src/components/App/Annotation.js
@@ -1,36 +1,12 @@
 import Tree from "../../core/Tree";
-import { isAlive, destroy } from "mobx-state-tree";
+import { isAlive } from "mobx-state-tree";
 import { useLayoutEffect } from "react";
 
 export function Annotation({ annotation, root }) {
   useLayoutEffect(() => {
     return () => {
       if (annotation && isAlive(annotation)) {
-        // Reset annotation state
-        annotation.resetReady();
-
-        // Clean up any observers or reactions
-        if (annotation.disposers) {
-          annotation.disposers.forEach((disposer) => disposer());
-        }
-
-        // Clean up any child regions
-        if (annotation.regions) {
-          annotation.regions.forEach((region) => {
-            if (isAlive(region)) {
-              destroy(region);
-            }
-          });
-        }
-
-        // Clean up any relations
-        if (annotation.relations) {
-          annotation.relations.forEach((relation) => {
-            if (isAlive(relation)) {
-              destroy(relation);
-            }
-          });
-        }
+        annotation.cleanup();
       }
     };
   }, [annotation?.pk, annotation?.id]);
