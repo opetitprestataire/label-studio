d969d4455918d6a204b7ce04cc4f1a102e2d864f
Brandon Martel <brandonmartel@gmail.com>
2025-05-16 08:34:04 -0500

    make app title nicer

--- web/apps/playground/src/components/PlaygroundApp/TopBar.tsx
+++ web/apps/playground/src/components/PlaygroundApp/TopBar.tsx
@@ -5,7 +5,9 @@ export const TopBar = memo(
   () => {
     return (
       <div className="flex items-center h-10 px-tight text-heading-medium justify-between select-none border-b border-neutral-border">
-        <span className="font-semibold tracking-tight text-body-medium">LabelStudio Playground</span>
+        <div className="flex items-center gap-2">
+          <span className="font-semibold tracking-tight text-body-medium">Label Studio <span className="text-accent-persimmon-base">Playground</span></span>
+        </div>
         <div className="flex items-center gap-2">
           <ThemeToggle />
         </div>
