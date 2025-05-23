6fab90f20bd6dc1a2267879fc5ec46157e784193
Gondragos <gondragos@gmail.com>
2025-05-23 22:22:15 +0100

    test: add behavior based wait

--- web/libs/editor/tests/e2e/tests/maxUsage.test.js
+++ web/libs/editor/tests/e2e/tests/maxUsage.test.js
@@ -166,7 +166,7 @@ Data(maxUsageImageToolsDataTable).Scenario(
   },
 );
 
-Data(maxUsageImageToolsDataTable).Scenario(
+Data(maxUsageImageToolsDataTable).only.Scenario(
   "Max usages of labels in ImageView on region creating",
   async ({ I, LabelStudio, AtImageView, AtOutliner, AtPanels, current }) => {
     const { maxUsage, shapeName } = current;
@@ -190,6 +190,7 @@ Data(maxUsageImageToolsDataTable).Scenario(
       I.pressKey(shape.hotkey);
       drawShapeByBbox(shape, 1 + 50 * k, 1, 30, 30, AtImageView);
       I.pressKey("u");
+      AtOutliner.seeRegions(k);
       I.waitTicks(2);
     }
 
