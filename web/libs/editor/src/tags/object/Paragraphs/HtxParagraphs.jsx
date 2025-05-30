0987411c0eb0c73da7e797eda1d64f537e2bb565
Brandon Martel <brandonmartel@gmail.com>
2025-05-30 07:38:12 -0500

    linting

--- web/libs/editor/src/tags/object/Paragraphs/HtxParagraphs.jsx
+++ web/libs/editor/src/tags/object/Paragraphs/HtxParagraphs.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useCallback } from "react";
+import React, { Component } from "react";
 import { inject, observer } from "mobx-react";
 
 import ObjectTag from "../../../components/Tags/Object";
