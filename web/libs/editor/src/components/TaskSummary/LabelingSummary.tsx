62686cec61c561bd75e897285c3f03fb1c81a0a4
nik <nik@heartex.net>
2025-05-30 19:04:44 -0700

    Align to the middle

--- web/libs/editor/src/components/TaskSummary/LabelingSummary.tsx
+++ web/libs/editor/src/components/TaskSummary/LabelingSummary.tsx
@@ -25,15 +25,17 @@ export const LabelingSummary = ({ annotations, controls }: Props) => {
       <tbody>
         {annotations.map((annotation) => (
           <tr key={annotation.id} className="odd:bg-neutral-surface">
-            <td className="px-4 py-2 whitespace-nowrap flex gap-tight items-center">
-              <Userpic
-                user={annotation.user}
-                className={annotation.type === "prediction" ? "!bg-accent-plum-subtle text-accent-plum-bold" : ""}
-              >
-                {annotation.type === "prediction" && <IconSparks style={{ width: 18, height: 18 }} />}
-              </Userpic>
-              <span>{annotation.user?.displayName ?? annotation.createdBy}</span>
-              <span>#{annotation.pk}</span>
+            <td className="px-4 py-2 whitespace-nowrap">
+              <div className="flex gap-tight items-center h-full">
+                <Userpic
+                  user={annotation.user}
+                  className={annotation.type === "prediction" ? "!bg-accent-plum-subtle text-accent-plum-bold" : ""}
+                >
+                  {annotation.type === "prediction" && <IconSparks style={{ width: 18, height: 18 }} />}
+                </Userpic>
+                <span>{annotation.user?.displayName ?? annotation.createdBy}</span>
+                <span>#{annotation.pk}</span>
+              </div>
             </td>
             {controls.map((control) => {
               // display only submitted results, not the current draft
