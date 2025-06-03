00abde69aec41ba7dc8f1fa347fc1a2c9cf1db44
hlomzik <hlomzik@gmail.com>
2025-06-03 15:52:50 +0100

    Also apply scrolling styles to DataSummary

--- web/libs/editor/src/components/TaskSummary/DataSummary.tsx
+++ web/libs/editor/src/components/TaskSummary/DataSummary.tsx
@@ -8,25 +8,27 @@ type DataSummaryProps = {
 
 export const DataSummary = ({ data_types, data }: DataSummaryProps) => {
   return (
-    <table className="table-auto border border-neutral-border rounded-small border-collapse">
-      <thead>
-        <tr className="bg-neutral-surface">
-          {Object.entries(data_types).map(([field, { type }]) => (
-            <th key={field} className="px-4 py-2 text-left whitespace-nowrap">
-              {field} <SummaryBadge>{type}</SummaryBadge>
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          {Object.entries(data_types).map(([field, { value }]) => (
-            <td key={field} className="px-4 py-2 align-top">
-              {typeof value === "object" ? JSON.stringify(value) : value}
-            </td>
-          ))}
-        </tr>
-      </tbody>
-    </table>
+    <div className="overflow-x-auto pb-tight mb-base">
+      <table className="table-auto border border-neutral-border rounded-small border-collapse">
+        <thead>
+          <tr className="bg-neutral-surface">
+            {Object.entries(data_types).map(([field, { type }]) => (
+              <th key={field} className="px-4 py-2 text-left whitespace-nowrap">
+                {field} <SummaryBadge>{type}</SummaryBadge>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {Object.entries(data_types).map(([field, { value }]) => (
+              <td key={field} className="px-4 py-2 align-top">
+                {typeof value === "object" ? JSON.stringify(value) : value}
+              </td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
