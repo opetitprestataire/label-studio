2df7074b8dfabe7cdd2499764ea0e4bad7a9da34
Marcel Canu <marcel@humansignal.com>
2025-06-04 17:33:17 -0300

    Added deserialize to test

--- label_studio/core/tests/test_models.py
+++ label_studio/core/tests/test_models.py
@@ -1,4 +1,7 @@
+import json
+
 from core.models import DeletedRow
+from django.core import serializers
 from django.test import TestCase
 from organizations.models import Organization
 from organizations.tests.factories import OrganizationFactory
@@ -9,7 +12,29 @@ from tasks.tests.factories import TaskFactory
 
 
 class TestDeletedRow(TestCase):
-    def test_serialize_and_create(self):
+    def _assert_delete_and_restore_equal(self, drow, original):
+        original_dict = original.__dict__.copy()
+        original_id = original.id
+        original_dict.pop('_state')
+        original_created_at = original_dict.pop('created_at')
+        original_updated_at = original_dict.pop('updated_at')
+        original.delete()
+
+        for deserialized_object in serializers.deserialize('json', json.dumps([drow.data])):
+            deserialized_object.save()
+        new_object = original.__class__.objects.get(id=original_id)
+
+        new_dict = new_object.__dict__.copy()
+        new_dict.pop('_state')
+        new_created_at = new_dict.pop('created_at')
+        new_updated_at = new_dict.pop('updated_at')
+
+        assert new_dict == original_dict
+        # Datetime loses microsecond precision, so we can't compare them directly
+        assert abs((new_created_at - original_created_at).total_seconds()) < 0.001
+        assert abs((new_updated_at - original_updated_at).total_seconds()) < 0.001
+
+    def test_serialize_organization(self):
         organization = OrganizationFactory()
         drow = DeletedRow.serialize_and_create(organization, reason='reason', organization_id=organization.id)
         assert drow.row_id == organization.id
@@ -21,19 +46,25 @@ class TestDeletedRow(TestCase):
         assert drow.organization_id == organization.id
         assert drow.project_id is None
         assert drow.user_id is None
+        self._assert_delete_and_restore_equal(drow, organization)
 
-        project = ProjectFactory(organization=organization)
+    def test_serialize_project(self):
+        project = ProjectFactory()
         drow = DeletedRow.serialize_and_create(
-            project, reason='reason', organization_id=organization.id, project_id=project.id
+            project, reason='reason', organization_id=project.organization.id, project_id=project.id
         )
         assert drow.row_id == project.id
         assert drow.model == 'projects.project'
         assert drow.data['fields']['title'] == project.title
-        assert drow.data['fields']['organization'] == organization.id
+        assert drow.data['fields']['organization'] == project.organization.id
         assert drow.reason == 'reason'
-        assert drow.organization_id == organization.id
+        assert drow.organization_id == project.organization.id
         assert drow.project_id == project.id
+        self._assert_delete_and_restore_equal(drow, project)
 
+    def test_serialize_task(self):
+        organization = OrganizationFactory()
+        project = ProjectFactory(organization=organization)
         task = TaskFactory(project=project)
         drow = DeletedRow.serialize_and_create(
             task,
@@ -49,6 +80,7 @@ class TestDeletedRow(TestCase):
         assert drow.organization_id == organization.id
         assert drow.project_id == project.id
         assert drow.user_id == organization.created_by_id
+        self._assert_delete_and_restore_equal(drow, task)
 
     def test_bulk_serialize_and_create(self):
         organization_1 = OrganizationFactory()
