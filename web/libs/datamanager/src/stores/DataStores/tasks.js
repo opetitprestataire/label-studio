7a5518cec37e794454df7bf2267016501f65e70e
makseq <makseq@users.noreply.github.com>
2025-06-23 02:09:15 +0000

    Apply pre-commit linters
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/15813549101

--- web/libs/datamanager/src/stores/DataStores/tasks.js
+++ web/libs/datamanager/src/stores/DataStores/tasks.js
@@ -149,7 +149,7 @@ export const create = (columns) => {
         const isLabelStream = getRoot(self).SDK?.mode === "labelstream";
         const taskParams = { taskID };
         if (isLabelStream) {
-          taskParams.interaction = 'labelstream';
+          taskParams.interaction = "labelstream";
         }
 
         const taskData = yield self.root.apiCall("task", taskParams);
