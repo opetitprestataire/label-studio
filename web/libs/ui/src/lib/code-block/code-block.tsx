da560fafb74b6926be7dfb11590592c357f70b2b
Ricardo Cabral <ricardo.cabralm@gmail.com>
2025-06-27 16:41:35 -0400

    Adds support for warning/negative variants to CodeBlock component.

--- web/libs/ui/src/lib/code-block/code-block.tsx
+++ web/libs/ui/src/lib/code-block/code-block.tsx
@@ -1,14 +1,24 @@
 export function CodeBlock({
   code,
   className,
+  variant = "default",
 }: {
   title?: string;
   description?: string;
   code: string;
   className?: string;
+  variant?: "default" | "warning" | "negative";
 }) {
+  const variantStyles = {
+    default: "bg-neutral-surface border-neutral-border",
+    warning: "bg-warning-background border-warning-border-subtle",
+    negative: "bg-negative-background border-negative-border-subtle",
+  };
+
   return (
-    <div className={`whitespace-pre-wrap font-mono mt-2 p-3 bg-neutral-surface rounded-md ${className || ""}`}>
+    <div
+      className={`whitespace-pre-wrap font-mono mt-2 p-3 rounded-md border  scrollbar-thin scrollbar-thumb-neutral-border-bold scrollbar-track-transparent ${variantStyles[variant]} ${className || ""}`}
+    >
       {code.trim()}
     </div>
   );
