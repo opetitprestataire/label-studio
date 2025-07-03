e9c6821c459dc2e22d3203fb876ec2580647ea00
Brandon Martel <brandonmartel@gmail.com>
2025-07-03 17:42:26 -0500

    enable all the tests again

--- web/libs/editor/cypress.config.ts
+++ web/libs/editor/cypress.config.ts
@@ -1,9 +1,3 @@
 import configure from "@humansignal/frontend-test/configure";
 
-export default configure((config) => {
-  // Only run the video regions test file
-  if (config.e2e) {
-    config.e2e.specPattern = "**/video/regions.cy.ts";
-  }
-  return config;
-});
+export default configure();
