b0b21a1b1916d59785b12cf272d042b2ef6d1c2b
Brandon Martel <brandonmartel@gmail.com>
2025-07-03 14:13:57 -0500

    testing whether these tests will pass when run as only suite

--- web/libs/editor/cypress.config.ts
+++ web/libs/editor/cypress.config.ts
@@ -1,3 +1,9 @@
 import configure from "@humansignal/frontend-test/configure";
 
-export default configure();
+export default configure((config) => {
+  // Only run the video regions test file
+  if (config.e2e) {
+    config.e2e.specPattern = "**/video/regions.cy.ts";
+  }
+  return config;
+});
