a04424480d18b6320528b77ccacf890bd45d5262
Ricardo Cabral <ricardo.cabralm@gmail.com>
2025-07-09 12:12:15 -0400

    Adds spacing between storage summager and storage sync button.

--- web/apps/labelstudio/src/pages/Settings/StorageSettings/StorageCard.jsx
+++ web/apps/labelstudio/src/pages/Settings/StorageSettings/StorageCard.jsx
@@ -63,7 +63,7 @@ export const StorageCard = ({ rootClass, target, storage, onEditStorage, onDelet
         storageTypes={storageTypes}
       />
       <div className={rootClass.elem("sync")}>
-        <div>
+        <div className="mt-base">
           <Button
             look="outlined"
             waiting={syncing}
