66d5ba32d1e88a768138c2db323718a8611f5504
Gondragos <gondragos@gmail.com>
2025-07-09 21:26:31 +0100

    test: Switch on buffering ff in all tests

--- web/libs/editor/tests/integration/feature-flags.ts
+++ web/libs/editor/tests/integration/feature-flags.ts
@@ -3,4 +3,5 @@ import * as FLAGS from "../../src/utils/feature-flags";
 export const CURRENT_FLAGS = {
   [FLAGS.FF_LSDV_4930]: true,
   [FLAGS.FF_LSDV_4620_3_ML]: false,
+  fflag_fix_front_fit_31_synced_media_buffering: true,
 };
