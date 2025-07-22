01ae0777ec91fc385619e3b7db58717ca04450ad
hlomzik <hlomzik@users.noreply.github.com>
2025-07-22 08:57:06 +0000

    Apply pre-commit linters
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/16439512962

--- web/libs/ui/src/lib/label/label.tsx
+++ web/libs/ui/src/lib/label/label.tsx
@@ -13,42 +13,41 @@ type LabelProps = PropsWithChildren<{
   flat?: boolean;
 }>;
 
-export const Label = (
-  {
-    text,
-    children,
-    required,
-    placement = "left",
-    className,
-    description,
-    size = "small",
-    style: inlineStyle,
-    simple,
-    flat,
-  }: LabelProps) => {
-    const TagName = simple ? "div" : "label";
+export const Label = ({
+  text,
+  children,
+  required,
+  placement = "left",
+  className,
+  description,
+  size = "small",
+  style: inlineStyle,
+  simple,
+  flat,
+}: LabelProps) => {
+  const TagName = simple ? "div" : "label";
 
-    return (
-      <TagName
-        style={inlineStyle}
-        data-required={required}
-        className={clsx(styles.label, className, {
-          [styles.label_size_small]: size === "small",
-          [styles.label_size_large]: size === "large",
-          [styles.label_flat]: flat,
-          [styles.label_placement_left]: placement === "left",
-          [styles.label_placement_right]: placement === "right",
-          [styles.label_withDescription]: !!description,
-          [styles.label_empty]: !children,
-        })}
-      >
-        <span className={clsx(styles.label__text)}>
-          <span className={clsx(styles.label__content)}>
-            {text}
-            {description && <span className={clsx(styles.label__description)}>{description}</span>}
-          </span>
+  return (
+    <TagName
+      style={inlineStyle}
+      data-required={required}
+      className={clsx(styles.label, className, {
+        [styles.label_size_small]: size === "small",
+        [styles.label_size_large]: size === "large",
+        [styles.label_flat]: flat,
+        [styles.label_placement_left]: placement === "left",
+        [styles.label_placement_right]: placement === "right",
+        [styles.label_withDescription]: !!description,
+        [styles.label_empty]: !children,
+      })}
+    >
+      <span className={clsx(styles.label__text)}>
+        <span className={clsx(styles.label__content)}>
+          {text}
+          {description && <span className={clsx(styles.label__description)}>{description}</span>}
         </span>
-        <span className={clsx(styles.label__field)}>{children}</span>
-      </TagName>
-    );
-  };
+      </span>
+      <span className={clsx(styles.label__field)}>{children}</span>
+    </TagName>
+  );
+};
