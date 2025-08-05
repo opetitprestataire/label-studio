a4bceb7808bc17e0686f146b94b89edd833fbbb4
nik <nik@heartex.net>
2025-08-05 17:07:47 +0300

    fix: BROS-277: Fix DM crash with empty shortcut

--- web/libs/editor/src/core/Hotkey.ts
+++ web/libs/editor/src/core/Hotkey.ts
@@ -456,7 +456,7 @@ Hotkey.Tooltip = inject("store")(
       const description = props.title ?? hotkey.description;
       const hotkeys: JSX.Element[] = [];
 
-      if (enabled) {
+      if (enabled && isDefined(shortcut)) {
         shortcut.split(",").forEach((combination: string) => {
           const keys = combination.split("+").map((key: string) =>
             createElement(
