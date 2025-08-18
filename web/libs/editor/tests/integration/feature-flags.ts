2c8a21a8b868b53e529e5599d7063c2a86afde3b
Gondragos <gondragos@gmail.com>
2025-08-18 06:30:16 +0100

    Set flag for tests as default

--- web/libs/editor/tests/integration/feature-flags.ts
+++ web/libs/editor/tests/integration/feature-flags.ts
@@ -3,4 +3,5 @@ import * as FLAGS from "../../src/utils/feature-flags";
 export const CURRENT_FLAGS = {
   [FLAGS.FF_LSDV_4930]: true,
   [FLAGS.FF_LSDV_4620_3_ML]: false,
+  fflag_feat_front_bros_327_multiple_labels_regions_short: true,
 };
