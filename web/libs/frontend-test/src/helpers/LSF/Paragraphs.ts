cb929d262410b918e52ef9254b73f2eb2cf3214b
Gondragos <gondragos@gmail.com>
2025-08-18 03:23:11 +0100

    test: Add test for create regions with multiple label blocks

--- web/libs/frontend-test/src/helpers/LSF/Paragraphs.ts
+++ web/libs/frontend-test/src/helpers/LSF/Paragraphs.ts
@@ -12,6 +12,60 @@ class ParagraphsHelper {
   get root() {
     return cy.get(this._rootSelector);
   }
+
+  phrase(index: number) {
+    return this.root.find(`[data-testid="phrase:${index}")`);
+  }
+
+  selectText(text: string, phraseIdx?: number) {
+    const target = phraseIdx !== undefined ? this.phrase(phraseIdx) : this.root;
+
+    return target.contains(text).then(($el) => {
+      const el = $el[0];
+      const textElement = el.childNodes[0];
+      const startOffset = el.textContent.indexOf(text);
+      const endOffset = startOffset + text.length;
+      const document = el.ownerDocument;
+      const range = document.createRange();
+      range.setStart(textElement, startOffset);
+      range.setEnd(textElement, endOffset);
+      this._selectRange(range);
+    });
+  }
+
+  _selectRange(range: Range) {
+    const el: HTMLElement = (
+      range.commonAncestorContainer.nodeType === Node.TEXT_NODE
+        ? range.commonAncestorContainer.parentElement
+        : range.commonAncestorContainer
+    ) as HTMLElement;
+    const elRect = el.getBoundingClientRect();
+    const startEdgeRange = range.cloneRange();
+    startEdgeRange.setEnd(range.startContainer, range.startOffset);
+    const endEdgeRange = range.cloneRange();
+    endEdgeRange.setStart(range.endContainer, range.endOffset);
+    const startRect = startEdgeRange.getBoundingClientRect();
+    const endRect = endEdgeRange.getBoundingClientRect();
+    const x = startRect.left - elRect.left;
+    const y = startRect.top - elRect.top;
+    const x2 = endRect.right - elRect.left;
+    const y2 = endRect.bottom - elRect.top;
+    const eventOptions = {
+      eventConstructor: "MouseEvent",
+      buttons: 1,
+      force: true,
+    };
+    cy.wrap(el)
+      .trigger("mousedown", x, y, eventOptions)
+      .trigger("mousemove", x2, y2, eventOptions)
+      .then(() => {
+        const document = el.ownerDocument;
+        const selection = document.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+      })
+      .trigger("mouseup", x2, y2, eventOptions);
+  }
 }
 
 const Paragraphs = new ParagraphsHelper("&:eq(0)");
