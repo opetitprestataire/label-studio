03098b849661327abd232640b46066db995f18f6
Sergei Ivashchenko <triklozoid@gmail.com>
2025-08-27 11:52:04 +0200

    Add feature flag

--- label_studio/core/utils/iterators.py
+++ label_studio/core/utils/iterators.py
@@ -1,3 +1,4 @@
+from core.feature_flags import flag_set
 from django.conf import settings
 
 
@@ -7,6 +8,11 @@ def iterate_queryset(queryset, chunk_size=None):
 
     if chunk_size <= 0:
         raise ValueError(f'chunk_size must be positive, got {chunk_size}')
+
+    if not flag_set('fflag_fix_back_plt_863_remove_iterator_27082025_short', user='auto'):
+        for obj in queryset.iterator(chunk_size=chunk_size):
+            yield obj
+        return
 
     model = queryset.model
     pk_field = model._meta.pk.name
