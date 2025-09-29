7507840fa7b6c4d8e71b0f46314ce76a016f42e5
hlomzik <hlomzik@gmail.com>
2025-09-29 21:09:01 +0100

    Remove HtxCustomRegionView, it's useless

--- web/libs/editor/src/tags/custom/CustomRegion.js
+++ web/libs/editor/src/tags/custom/CustomRegion.js
@@ -1,15 +1,13 @@
 import { observer } from "mobx-react";
 import { types, getParent } from "mobx-state-tree";
-
-import NormalizationMixin from "../../mixins/Normalization";
-import { AreaMixin } from "../../mixins/AreaMixin";
-import RegionsMixin from "../../mixins/Regions";
-import { guidGenerator } from "../../core/Helpers";
-
-import { HtxTextBox } from "../../components/HtxTextBox/HtxTextBox";
-import { cn } from "../../utils/bem";
-import { CustomInterfaceModel } from "./Custom";
 import { IconGrid } from "@humansignal/icons";
+
+import { AreaMixin } from "../../mixins/AreaMixin";
+import NormalizationMixin from "../../mixins/Normalization";
+import RegionsMixin from "../../mixins/Regions";
+import { CustomInterfaceModel } from "./Custom";
+
+import { cn } from "../../utils/bem";
 
 const Model = types
   .model("CustomRegionModel", {
@@ -62,75 +60,6 @@ const Model = types
   }));
 
 const CustomRegionModel = types.compose("CustomRegionModel", RegionsMixin, AreaMixin, NormalizationMixin, Model);
-
-const HtxCustomRegionView = ({ item, onFocus }) => {
-  const classes = [styles.mark];
-  const params = { onFocus: (e) => onFocus(e, item) };
-  const { parent } = item;
-  const { relationMode } = item.annotation;
-  const editable = parent.isEditable && !item.isReadOnly();
-  const deleteable = parent.isDeleteable && !item.isReadOnly();
-
-  if (relationMode) {
-    classes.push(styles.relation);
-  }
-
-  if (item.selected) {
-    classes.push(styles.selected);
-  } else if (item.highlighted) {
-    classes.push(styles.highlighted);
-  }
-
-  if (editable || parent.transcription) {
-    params.onChange = (str) => {
-      item.setValue(str);
-      item.parent.updateLeadTime();
-    };
-    params.onInput = () => {
-      item.parent.countTime();
-    };
-  }
-
-  params.onDelete = item.deleteRegion;
-
-  let divAttrs = {};
-
-  if (!parent.perregion) {
-    divAttrs = {
-      onMouseOver: () => {
-        if (relationMode) {
-          item.setHighlight(true);
-        }
-      },
-      onMouseOut: () => {
-        if (relationMode) {
-          item.setHighlight(false);
-        }
-      },
-    };
-  }
-
-  const name = `${parent?.name ?? ""}:${item.id}`;
-
-  return (
-    <div {...divAttrs} className={cn("row").toString()} data-testid="custom-region">
-      <HtxTextBox
-        isEditable={editable}
-        isDeleteable={deleteable}
-        onlyEdit={parent.transcription}
-        id={`CustomRegion-${item.id}`}
-        name={name}
-        className={classes.join(" ")}
-        rows={parent.rows}
-        text={item._value}
-        {...params}
-        ignoreShortcuts={true}
-      />
-    </div>
-  );
-};
-
-const HtxCustomRegion = observer(HtxCustomRegionView);
 
 // required for NodeViews
 CustomRegionModel.nodeView = {
